9e035547112077daecf1f629d4ba0b6a46998451
kvarcas91 <e.slutas@gmail.com>
2022-04-24 16:07:53 +0100

    s

--- HRTools_v2/DPI/DpiDecorator.cs
+++ HRTools_v2/DPI/DpiDecorator.cs
@@ -12,7 +12,7 @@ namespace HRTools_v2.DPI
             {
 
                 Matrix m = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice;
-                if (m.M11 > 1.5 && m.M22 > 1.5) return;
+                //if (m.M11 > 1.5 && m.M22 > 1.5) return;
                 ScaleTransform dpiTransform = new ScaleTransform(1 / m.M11, 1 / m.M22);
                 if (dpiTransform.CanFreeze)
                     dpiTransform.Freeze();
